13b9b359e4cdc4280df98113d5631f2dc057c687
soul <ykrainec1991@gmail.com>
2020-05-23 14:42:22 +0200

    Display BookList

--- js/app.js
+++ js/app.js
@@ -1,2 +1,27 @@
 $(function () {
+    var bookListDiv = $(".book-list");
+
+    refreshBookList();
+
+    function refreshBookList() {
+        // var bookArr = getFromServer();
+        renderBookList(bookListDiv);
+    }
+
+    function renderBookList(renderingPoint) {
+        $.ajax({
+            url : "http://localhost:8282/books",
+            type : "GET",
+            dataType : "json",
+        }).done(function (booksArr) {
+            renderingPoint.empty();
+            for (var i = 0; i < booksArr.length; i++){
+                var titleDiv = $('<div class="book-title">');
+                titleDiv.text(booksArr[i].title);
+                renderingPoint.append(titleDiv);
+            }
+        }).fail(function (xhr, status, err) {
+            console.log(xhr, status, err);
+        });
+    }
 })
